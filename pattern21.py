6f0b114230d6954dee8856eaf23b81f4339ea27a
tejasree <tejasreeboyina@gmail.com>
2023-07-12 06:41:11 +0530

    6th day pattern programs practice

---
+++ pattern21.py
@@ -0,0 +1,15 @@
+# * 
+# * * 
+# * * * 
+# * * * * 
+# * * * * * 
+# * * * * 
+# * * * 
+# * * 
+# * 
+
+n = int(input())
+for i in range(n):
+    print((i+1) * "* ")
+for j in range(n,0,-1):
+    print((j-1) * "* ")
